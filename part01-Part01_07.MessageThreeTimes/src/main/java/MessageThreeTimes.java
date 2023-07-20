8726781f0add8b390334cf83015a6650e6d94356
Bramwel79 <140046556+Bramwel79@users.noreply.github.com>
2023-07-20 10:09:46 +0300

    Add files via upload

---
+++ part01-Part01_07.MessageThreeTimes/src/main/java/MessageThreeTimes.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class MessageThreeTimes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        System.out.println("Write a message:");
+        // Write your program here
+        input = scanner.nextLine();
+        System.out.println(input);
+        System.out.println(input);
+        System.out.println(input);
+
+    }
+}
